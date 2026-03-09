29e0889af71543e8b5ec9c782f1db98d4ff222e9
Nick Chomey <nick_chomey@hotmail.com>
2026-03-09 08:56:26 -0600

    .

--- src/plugins/flexsearch/indexing.ts
+++ src/plugins/flexsearch/indexing.ts
@@ -93,6 +93,9 @@ export async function catchUpFromCheckpoint<RxDocType, Internals, InstanceCreati
 ): Promise<boolean> {
     let checkpoint = startCheckpoint;
     let hasChanges = false;
+    let totalDocsProcessed = 0;
+
+    console.log('[FlexSearch Catchup] Starting with checkpoint:', startCheckpoint);
 
     while (true) {
         const changed = await getChangedDocumentsSince(
@@ -102,6 +105,8 @@ export async function catchUpFromCheckpoint<RxDocType, Internals, InstanceCreati
         );
         if (changed.documents.length > 0) {
             hasChanges = true;
+            totalDocsProcessed += changed.documents.length;
+            console.log('[FlexSearch Catchup] Processing', changed.documents.length, 'documents, total:', totalDocsProcessed);
             changed.documents.forEach(documentData => {
                 const primaryValue = getPrimaryValue(documentData, state.primaryPath);
                 if (!primaryValue) {
